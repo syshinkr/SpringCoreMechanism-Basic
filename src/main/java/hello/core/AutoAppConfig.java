27401692ffd281316ebd7bbc3f0be75dd62df626
syshinkr <syshinkr228@gmail.com>
2023-02-24 09:25:09 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 // @ComponentScan: @Component 가 붙은 클래스를 스프링빈으로 등록한다
+// Default scan scope: 해당 클래스가 속한 패키지부터 그 하위 패키지까지 스캔한다
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
